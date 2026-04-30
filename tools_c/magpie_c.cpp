5b786108d881bc9861788b857e5ab0d12cea4646
Tobias Weber (Institut Laue-Langevin) <tweber@ill.fr>
2026-04-30 07:55:20 +0200

    Update magpie_c.cpp

--- tools_c/magpie_c.cpp
+++ tools_c/magpie_c.cpp
@@ -114,7 +114,7 @@ static t_E_and_S _calc_energies(t_magpie _mag,
 			// use cached value if within epsilon distance
 			if(tl2::norm(Q_rlu - nearest_point) < dat->rtree_rlu_eps)
 			{
-				std::cout << "Using cached value at Q = (" << h << ", " << k << ", " << l << ")." << std::endl;
+				//std::cout << "Using cached value at Q = (" << h << ", " << k << ", " << l << ")." << std::endl;
 				return std::get<1>(rtree_nearest[0]);
 			}
 		}
